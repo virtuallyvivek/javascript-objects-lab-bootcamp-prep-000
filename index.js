2d0910ebbde41d14462818c196304d5e57649f0c
Catherine Straus <vnair611@gmail.com>
2020-06-23 00:07:05 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,16 @@
+var obj = { foo: 'bar' }
+ 
+var newObj = Object.assign({}, obj)
+ 
+newObj // { foo: 'bar' }
+ 
+delete newObj.foo // true
+ 
+newObj // {}
+ 
+obj // { foo: 'bar' }
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return obj
+  return Object.assign({}, obj, )
+}
