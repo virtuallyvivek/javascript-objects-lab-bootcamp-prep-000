889b5001394ab9af2f0acbc934d7325cd364a19b
Catherine Straus <vnair611@gmail.com>
2020-06-23 00:18:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  object[key] = value
-  return object
+  return Object.assign({}, object)
 }
 
 function deleteFromObjectByKey(object, key){
